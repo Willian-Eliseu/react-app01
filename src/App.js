e44095599394bbe31802871a0592d5ed1569ba03
Willian Eliseu da Silva <w.eliseu@gmail.com>
2024-10-07 12:01:00 -0300

    tic-tac-toe - 12h - 2024 oct 7

--- src/App.js
+++ src/App.js
@@ -30,6 +30,42 @@ export default App;
 */
 
 //TIC-TAC-TOE
-export default function Square(){
-	return <button className="square">X</button>;
+function Square(){
+	const [value, setValue] = useState(null);
+
+	function handleClick(){
+		setValue('X');
+	}
+
+	return(
+		<button 
+			className="square"
+			onClick={handleClick}
+		>
+			{value}
+		</button>
+	);
+}
+
+
+export default function Board(){
+	return (
+		<>
+			<div className="board-row">
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div className="board-row">
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div className="board-row">
+				<Square />
+				<Square />
+				<Square />
+			</div>
+		</>
+	);
 }
