67a130833dc7ea002ae4dfc71eae440718ffa7e4
Willian Eliseu da Silva <w.eliseu@gmail.com>
2024-10-07 10:00:59 -0300

    update app.js - tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import Header from './components/header';
-import Corpo from './components/Corpo';
-import Dados from './components/Dados';
-import Botao from './components/botao';
+// import Header from './components/header';
+// import Corpo from './components/Corpo';
+//import Dados from './components/Dados';
+//import Botao from './components/botao';
 import './App.css';
 
+/*
 function App() {
 	
 	const [count, setCount] = useState(0);
@@ -26,3 +27,9 @@ function App() {
 }
 
 export default App;
+*/
+
+//TIC-TAC-TOE
+export default function Square(){
+	return <button className="square">X</button>;
+}
